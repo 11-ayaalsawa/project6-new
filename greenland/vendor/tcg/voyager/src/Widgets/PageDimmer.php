79bc1b57762ffad8d932b23211428c5d86f0760d
10-Anas <anasq0q@gmail.com>
2022-06-14 23:23:12 +0300

    escape ignore

---
+++ greenland/vendor/tcg/voyager/src/Widgets/PageDimmer.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace TCG\Voyager\Widgets;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use TCG\Voyager\Facades\Voyager;
+use Illuminate\Support\Facades\DB;
+
+class PageDimmer extends BaseDimmer
+{
+    /**
+     * The configuration array.
+     *
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * Treat this method as a controller action.
+     * Return view() or other content to display.
+     */
+    public function run()
+    {
+        $aprrove=DB::select('select * from users where hold=1');
+        // $count = Voyager::model('Page')->count();
+        $count = count($aprrove);
+        // $string = trans_choice('voyager::dimmer.page', $count);
+        $string = 'Approves';
+
+        return view('voyager::dimmer', array_merge($this->config, [
+            'icon'   => 'voyager-file-text',
+            'title'  => "{$count} {$string}",
+            'text'   => __('voyager::dimmer.page_text', ['count' => $count, 'string' => Str::lower($string)]),
+            'button' => [
+                'text' => __('voyager::dimmer.page_link_text'),
+                // 'link' => route('voyager.pages.index'),
+                'link' => url('/admin/approve'),
+            ],
+            'image' => voyager_asset('images/widget-backgrounds/03.jpg'),
+        ]));
+    }
+
+    /**
+     * Determine if the widget should be displayed.
+     *
+     * @return bool
+     */
+    public function shouldBeDisplayed()
+    {
+        return Auth::user()->can('browse', Voyager::model('Page'));
+    }
+}
